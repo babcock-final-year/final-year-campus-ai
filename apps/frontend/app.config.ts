6eaccbbe1a6057602922d01ebe2140ed361226e5
Iceman8911 <wuchiJSS3@gmail.com>
2026-02-20 21:02:57 +0100

    chore(frontend, lib): add lucide icons

--- apps/frontend/app.config.ts
+++ apps/frontend/app.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
+import lucidePreprocess from "vite-plugin-lucide-preprocess";
 
 export default defineConfig({
 	vite: {
-		plugins: [tailwindcss()],
+		plugins: [lucidePreprocess(), tailwindcss()],
 	},
 });
