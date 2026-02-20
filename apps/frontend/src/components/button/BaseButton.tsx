480de7c84c77e7dfccac48483f7fc8bd52bdec34
Iceman8911 <wuchiJSS3@gmail.com>
2026-02-20 20:08:53 +0100

    fixup! feat(frontend, component): add base button component

--- apps/frontend/src/components/button/BaseButton.tsx
+++ apps/frontend/src/components/button/BaseButton.tsx
@@ -1,7 +1,14 @@
+import clsx from "clsx/lite";
 import type { ComponentProps } from "solid-js";
 
 interface BaseButtonProps extends ComponentProps<"button"> {}
 
 export default function BaseButton(props: BaseButtonProps) {
-	return <button {...props} type={props.type ?? "button"}></button>;
+	return (
+		<button
+			{...props}
+			class={clsx("btn", props.class)}
+			type={props.type ?? "button"}
+		></button>
+	);
 }
