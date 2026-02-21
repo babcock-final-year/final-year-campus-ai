8e25291c8b5b0949b359ab04fc8ca3519aea926e
Iceman8911 <wuchiJSS3@gmail.com>
2026-02-21 16:42:01 +0100

    feat(frontend, constant): add more regexes
    - upper case
    - lower case
    - number
    - symbol

--- apps/frontend/src/constants/regex.ts
+++ apps/frontend/src/constants/regex.ts
@@ -1,2 +1,10 @@
 // e.g 22/0039, 21/4321
 export const MATRIC_NUMBER_REGEX: RegExp = /^\d{2}\/\d{4}$/;
+
+export const UPPER_CASE_REGEX: RegExp = /\p{Lu}/u;
+
+export const LOWER_CASE_REGEX: RegExp = /\p{Ll}/u;
+
+export const NUMBER_REGEX: RegExp = /\p{Nd}/u;
+
+export const SYMBOL_REGEX: RegExp = /[^\p{L}\p{N}]/u;
