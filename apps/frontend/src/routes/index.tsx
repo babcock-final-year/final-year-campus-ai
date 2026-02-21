4e397ed129f7cf5c20c20d46fc5344702957b096
Iceman8911 <wuchiJSS3@gmail.com>
2026-02-21 01:51:22 +0100

    wip(frontend, style): use more suitable custom light-mode theme

--- apps/frontend/src/routes/index.tsx
+++ apps/frontend/src/routes/index.tsx
@@ -12,25 +12,27 @@ import AppLogo from "~/components/svg/AppLogo";
 function ListWithIcon(props: { children: JSXElement; icon: JSXElement }) {
 	return (
 		<li class="grid grid-cols-[1.5rem_1fr] place-items-center gap-6">
-			<div class="rounded-full bg-neutral/75 p-2 shadow">{props.icon}</div>
+			<div class="rounded-full bg-secondary p-2 text-primary-content shadow">
+				{props.icon}
+			</div>
 
-			{props.children}
+			<div class="text-primary-content">{props.children}</div>
 		</li>
 	);
 }
 
 export default function Home() {
 	return (
-		<div class="hero size-full overflow-auto bg-linear-to-b from-primary/40 via-33% to-primary/20 p-8">
+		<div class="hero size-full overflow-auto bg-linear-to-b from-sky-900 to-blue-950 p-8">
 			<div class="hero-content flex-col gap-6 text-center sm:flex-row">
-				<AppLogo class="*:hidden *:h-auto *:w-2xs *:rounded-lg *:fill-base-content *:text-shadow *:sm:block" />
+				<AppLogo class="*:hidden *:h-auto *:w-2xs *:rounded-lg *:fill-primary-content *:text-shadow *:sm:block" />
 
 				<div>
-					<AppLogo class="*:block *:h-auto *:w-24 *:place-self-center *:rounded-lg *:fill-base-content *:sm:hidden" />
+					<AppLogo class="*:block *:h-auto *:w-24 *:place-self-center *:rounded-lg *:fill-primary-content *:sm:hidden" />
 
-					<h1 class="py-4 font-bold text-5xl">Unipal</h1>
+					<h1 class="py-4 font-bold text-5xl text-primary-content">Unipal</h1>
 
-					<p class="py-4 font-semibold">
+					<p class="py-4 font-semibold text-primary-content">
 						Your intelligent assistant for campus life.
 					</p>
 
@@ -47,10 +49,10 @@ export default function Home() {
 					</ul>
 
 					<div class="flex flex-col items-center justify-center gap-4 py-4">
-						<BaseButton class="btn-primary btn-wide btn-lg">
+						<BaseButton class="btn-wide btn-lg [--btn-color:var(--color-primary-content)] [--btn-fg:var(--color-primary)]">
 							Get Started <UserRoundPlus />
 						</BaseButton>
-						<BaseButton class="btn-ghost btn-wide btn-lg">
+						<BaseButton class="btn-ghost btn-wide btn-lg text-primary-content hover:text-base-content">
 							Sign In <LogIn />
 						</BaseButton>
 					</div>
