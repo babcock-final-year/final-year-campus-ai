048c01ae3062a4b48abd88ff1fc4e804129f716c
Iceman8911 <wuchiJSS3@gmail.com>
2026-02-22 21:25:00 +0100

    chore(frontend, config): add route prerendering / SSG

--- apps/frontend/app.config.ts
+++ apps/frontend/app.config.ts
@@ -3,6 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 import lucidePreprocess from "vite-plugin-lucide-preprocess";
 
 export default defineConfig({
+	server: {
+		prerender: { crawlLinks: true },
+	},
 	vite: {
 		plugins: [lucidePreprocess(), tailwindcss()],
 	},
