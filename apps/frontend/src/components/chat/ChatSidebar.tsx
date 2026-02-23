3a2f498e17ed0f6a85d1811e2410e7590936cba8
Iceman8911 <wuchiJSS3@gmail.com>
2026-02-23 23:04:47 +0100

    wip(frontend, component): work on sidebar

--- apps/frontend/src/components/chat/ChatSidebar.tsx
+++ apps/frontend/src/components/chat/ChatSidebar.tsx
@@ -1,53 +1,79 @@
+import { Button } from "@kobalte/core/button";
+import { Link } from "@kobalte/core/link";
 import { TextField } from "@kobalte/core/text-field";
 import {
-	FilePenLine,
+	Ellipsis,
+	FilePen,
 	Folder,
 	MessageCircleMore,
 	Search,
 	Settings,
 } from "lucide-solid";
-import BaseButton from "../button/BaseButton";
+import { For } from "solid-js";
 
 export default function ChatSidebar() {
 	return (
-		<div class="flex h-screen w-2xs flex-col gap-4 overflow-auto px-2 py-8">
+		<div class="flex h-screen w-2xs flex-col gap-8 overflow-auto px-2 py-8">
 			<div class="flex items-center justify-center gap-4">
 				<TextField>
-					<TextField.Label class="input rounded-full bg-base-100">
+					<TextField.Label class="input input-secondary rounded-full bg-base-100">
 						<Search class="opacity-50" />
 
 						<TextField.Input placeholder="Search" type="search" />
 					</TextField.Label>
 				</TextField>
 
-				<FilePenLine class="size-8 text-secondary" />
+				<FilePen class="mr-2 min-w-6 text-secondary" />
 			</div>
 
-			<BaseButton class="btn-ghost mt-4 flex items-center justify-start p-2 font-semibold text-secondary">
-				<FilePenLine />
-				New Chat
-			</BaseButton>
+			<ul class="menu w-full px-0 [--menu-active-bg:var(--color-accent)] [--menu-active-fg:var(--color-accent-content)]">
+				<li>
+					<Button class="px-0 font-semibold text-secondary">
+						<FilePen />
+						New Chat
+					</Button>
+				</li>
 
-			<BaseButton class="btn-ghost flex items-center justify-start p-2 font-semibold text-secondary">
-				<Folder />
-				New Project
-			</BaseButton>
+				<li>
+					<Button class="px-0 font-semibold text-secondary">
+						<Folder />
+						New Project
+					</Button>
+				</li>
+			</ul>
 
-			<div class="mt-8 flex flex-col gap-4 text-secondary">
-				<h3 class="flex items-center gap-4 font-semibold">
+			<div class="flex flex-col text-secondary">
+				<h3 class="flex items-center gap-2 font-semibold">
 					<MessageCircleMore /> Recent Chats
 				</h3>
 
-				<ul class="list-none space-y-2 px-2 font-semibold text-sm *:truncate">
-					<li>How much is the average school fees </li>
-					<li>When is chapel seminar</li>
+				<ul class="menu w-full px-0 font-semibold text-sm [--menu-active-bg:var(--color-accent)] [--menu-active-fg:var(--color-accent-content)]">
+					<For
+						each={[
+							"Average School Fees for 2024 adsddsddqdwdq",
+							"What is Chapel Seminar?",
+						]}
+					>
+						{(chatSummary) => (
+							<li class="w-full">
+								<Link class="flex w-full gap-2" href="#">
+									<span class="grow truncate">{chatSummary}</span>
+									<Ellipsis class="min-w-6" />
+								</Link>
+							</li>
+						)}
+					</For>
 				</ul>
 			</div>
 
-			<BaseButton class="btn-ghost mt-auto flex items-center justify-start p-2 font-semibold text-secondary">
-				<Settings />
-				Settings & Help
-			</BaseButton>
+			<ul class="menu mt-auto w-full px-0 font-semibold [--menu-active-bg:var(--color-accent)] [--menu-active-fg:var(--color-accent-content)]">
+				<li>
+					<Button class="flex px-0 text-secondary">
+						<Settings />
+						Settings & Help
+					</Button>
+				</li>
+			</ul>
 		</div>
 	);
 }
