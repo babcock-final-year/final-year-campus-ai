4cc37dfadf33f71baf5fec7366adbedec868f25f
Iceman8911 <wuchiJSS3@gmail.com>
2026-02-24 00:18:00 +0100

    feat(frontend, component): add desktop responsiveness to chat side bar

--- apps/frontend/src/components/chat/ChatMainAreaHeader.tsx
+++ apps/frontend/src/components/chat/ChatMainAreaHeader.tsx
@@ -14,7 +14,7 @@ export default function ChatMainAreaHeader() {
 			<Drawer breakPoints={[0.75]} side="left">
 				{(drawerProps) => (
 					<>
-						<Drawer.Trigger class="btn btn-ghost btn-circle btn-sm bg-base-200">
+						<Drawer.Trigger class="btn btn-ghost btn-circle btn-sm bg-base-200 sm:hidden">
 							<Ellipsis class="stroke-primary" />
 						</Drawer.Trigger>
 
@@ -27,14 +27,14 @@ export default function ChatMainAreaHeader() {
 							/>
 							{/* The padding and negative left is to give clearance when the user drags on the drawer further rightwards, otherwise it'll look "cut-off" */}
 							<Drawer.Content class="fixed inset-0 -left-12 z-10 flex w-fit bg-accent pl-12 corvu-transitioning:transition-transform corvu-transitioning:duration-500">
-								<ChatSidebar />
+								<ChatSidebar isInDrawer={true} />
 							</Drawer.Content>
 						</Drawer.Portal>
 					</>
 				)}
 			</Drawer>
 
-			<div class="flex items-center justify-center gap-2 rounded-field bg-base-200 px-2 py-4 [&_svg]:stroke-primary">
+			<div class="ml-auto flex items-center justify-center gap-2 rounded-field bg-base-200 px-2 py-4 [&_svg]:stroke-primary">
 				<BaseButton class="btn-ghost btn-circle btn-sm">
 					<MessageCirclePlus />
 				</BaseButton>
