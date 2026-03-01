9ed9138b33291fe4418dddeae7be0766e92fd2d5
Iceman8911 <wuchiJSS3@gmail.com>
2026-03-01 16:13:59 +0100

    feat(frontend, component): add input focus to chat input field

--- apps/frontend/src/components/chat/ChatMainAreaFooter.tsx
+++ apps/frontend/src/components/chat/ChatMainAreaFooter.tsx
@@ -1,8 +1,16 @@
 import { TextField } from "@kobalte/core/text-field";
 import { Plus } from "lucide-solid";
+import { onMount } from "solid-js";
 import BaseButton from "../button/BaseButton";
 
 export default function ChatMainAreaFooter() {
+	let chatInput$!: HTMLInputElement;
+
+	onMount(() => {
+		// Focus on the input field for ux
+		chatInput$.focus();
+	});
+
 	return (
 		<div class="flex items-center justify-center gap-4">
 			<BaseButton class="btn-secondary btn-circle">
@@ -13,7 +21,11 @@ export default function ChatMainAreaFooter() {
 				<TextField.Label class="input input-primary w-full">
 					<span class="label text-primary/75">Ask</span>
 
-					<TextField.Input placeholder="Anything!" type="text" />
+					<TextField.Input
+						placeholder="Anything!"
+						ref={chatInput$}
+						type="text"
+					/>
 				</TextField.Label>
 			</TextField>
 		</div>
