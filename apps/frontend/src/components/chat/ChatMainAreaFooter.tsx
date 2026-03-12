f470bfbe35a808632d7b4a92baeb435cdd770f77
Iceman8911 <wuchiJSS3@gmail.com>
2026-03-12 14:12:19 +0100

    fixup! feat(frontend, component): add input focus to chat input field

--- apps/frontend/src/components/chat/ChatMainAreaFooter.tsx
+++ apps/frontend/src/components/chat/ChatMainAreaFooter.tsx
@@ -4,13 +4,6 @@ import { onMount } from "solid-js";
 import BaseButton from "../ui/button/BaseButton";
 
 export default function ChatMainAreaFooter() {
-	let chatInput$!: HTMLInputElement;
-
-	onMount(() => {
-		// Focus on the input field for ux
-		chatInput$.focus();
-	});
-
 	return (
 		<TextField class="items-center-safe flex flex-col gap-4 pt-2">
 			<TextField.Label class="input h-12 w-5/6 max-w-3xl border-transparent shadow outline-2 outline-primary/25">
@@ -19,8 +12,8 @@ export default function ChatMainAreaFooter() {
 				</BaseButton>
 
 				<TextField.Input
+					autofocus
 					placeholder="Ask Unipal anything..."
-					ref={chatInput$}
 					type="text"
 				/>
 
