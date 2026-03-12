528152fb0def29b02f38f4be8a291fe81c5721e7
Iceman8911 <wuchiJSS3@gmail.com>
2026-03-12 14:39:10 +0100

    wip(frontend, route): add complaint form

--- apps/frontend/src/routes/home/settings/complaint.tsx
+++ apps/frontend/src/routes/home/settings/complaint.tsx
@@ -1,15 +1,26 @@
 import {
 	createForm,
+	Field,
 	Form,
+	getInput,
+	reset,
 	type SubmitEventHandler,
 	SubmitHandler,
 } from "@formisch/solid";
-import { FileComplaintFormSchema } from "~/models/file-complaint-form";
+import FieldTextInput from "~/components/form/FieldTextInput";
+import BaseButton from "~/components/ui/button/BaseButton";
+import LimitCounter from "~/components/ui/indicator/LimitCounter";
+import {
+	FILE_COMPLAINT_FORM_DESCRIPTION_MAX_LENGTH,
+	FILE_COMPLAINT_FORM_TITLE_MAX_LENGTH,
+	FileComplaintFormSchema,
+} from "~/models/file-complaint-form";
 
 export default function SettingsInterfaceComplaintPage() {
 	const fileComplaintForm = createForm({
 		initialInput: { description: "", title: "" },
 		schema: FileComplaintFormSchema,
+		validate: "input",
 	});
 
 	const handleSubmitFileComplaintForm: SubmitEventHandler<
@@ -26,6 +37,70 @@ export default function SettingsInterfaceComplaintPage() {
 		>
 			<h2 class="font-semibold">File Complaint</h2>
 			<div class="divider my-2" />
+
+			<div class="flex grow flex-col gap-6 p-2">
+				<Field of={fileComplaintForm} path={["title"]}>
+					{(field) => (
+						<FieldTextInput
+							{...field}
+							icon=""
+							inputClass="bg-base-200 max-w-full w-full"
+							label={
+								<h4 class="flex">
+									Complaint Title
+									<LimitCounter
+										class="ml-auto"
+										max={FILE_COMPLAINT_FORM_TITLE_MAX_LENGTH}
+										val={
+											getInput(fileComplaintForm, { path: ["title"] })?.length
+										}
+									/>
+								</h4>
+							}
+							placeholder="Brief summary of the issue"
+							type="text"
+						/>
+					)}
+				</Field>
+
+				<Field of={fileComplaintForm} path={["description"]}>
+					{(field) => (
+						<FieldTextInput
+							{...field}
+							icon=""
+							inputClass="bg-base-200 max-w-full w-full h-32 resize-none"
+							label={
+								<h4 class="flex">
+									Detailed Description
+									<LimitCounter
+										class="ml-auto"
+										max={FILE_COMPLAINT_FORM_DESCRIPTION_MAX_LENGTH}
+										val={
+											getInput(fileComplaintForm, { path: ["description"] })
+												?.length
+										}
+									/>
+								</h4>
+							}
+							placeholder="Please provide as much detail as possible..."
+							type="textarea"
+						/>
+					)}
+				</Field>
+
+				<div class="flex gap-4">
+					<BaseButton class="btn-primary" type="submit">
+						Send Report
+					</BaseButton>
+					<BaseButton
+						class="btn-ghost"
+						onClick={() => reset(fileComplaintForm)}
+						type="reset"
+					>
+						Cancel
+					</BaseButton>
+				</div>
+			</div>
 		</Form>
 	);
 }
