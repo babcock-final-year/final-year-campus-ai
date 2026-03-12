829d8d8c5b0016cd84b407f1a264e554dbf123fc
Iceman8911 <wuchiJSS3@gmail.com>
2026-03-12 03:18:29 +0100

    wip(frontend, component): tweak settings interface padding

--- apps/frontend/src/routes/home/settings.tsx
+++ apps/frontend/src/routes/home/settings.tsx
@@ -10,7 +10,7 @@ export default function SettingsInterfaceLayout(props: {
 		<div class="grid size-full grid-rows-[3.5rem_1fr]">
 			<HomeMainAreaHeader />
 
-			<div class="grid grid-rows-8 gap-4 overflow-auto bg-base-200 p-4 sm:grid-cols-[16rem_1fr_1fr]">
+			<div class="grid grid-rows-8 gap-4 overflow-auto bg-base-200 p-8 sm:grid-cols-[16rem_1fr_1fr]">
 				<SettingsProfileSummaryCard class="col-start-1 col-end-4 row-start-1 row-end-3" />
 
 				<SettingsActionMenu class="sm:col-start-1 sm:col-end-2 sm:row-start-3 sm:row-end-9" />
