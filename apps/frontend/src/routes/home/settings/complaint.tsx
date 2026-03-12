f753a80d9299457930930096f26f895b218508aa
Iceman8911 <wuchiJSS3@gmail.com>
2026-03-12 10:29:39 +0100

    wip(frontend, route): add complaint form page

--- apps/frontend/src/routes/home/settings/complaint.tsx
+++ apps/frontend/src/routes/home/settings/complaint.tsx
@@ -1,3 +1,31 @@
+import {
+	createForm,
+	Form,
+	type SubmitEventHandler,
+	SubmitHandler,
+} from "@formisch/solid";
+import { FileComplaintFormSchema } from "~/models/file-complaint-form";
+
 export default function SettingsInterfaceComplaintPage() {
-	return "";
+	const fileComplaintForm = createForm({
+		initialInput: { description: "", title: "" },
+		schema: FileComplaintFormSchema,
+	});
+
+	const handleSubmitFileComplaintForm: SubmitEventHandler<
+		typeof FileComplaintFormSchema
+	> = (formData, e) => {
+		// TODO: Sumbit file complaint
+	};
+
+	return (
+		<Form
+			class="flex size-full flex-col rounded-box border border-base-300 bg-secondary p-4"
+			of={fileComplaintForm}
+			onSubmit={handleSubmitFileComplaintForm}
+		>
+			<h2 class="font-semibold">File Complaint</h2>
+			<div class="divider my-2" />
+		</Form>
+	);
 }
