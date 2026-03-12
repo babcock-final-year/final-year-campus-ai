9fca2621a5e07a3f49817d4bde15b68a4b98b0af
Iceman8911 <wuchiJSS3@gmail.com>
2026-03-12 21:07:54 +0100

    wip(frontend, route): add profile view

--- apps/frontend/src/routes/home/settings/profile.tsx
+++ apps/frontend/src/routes/home/settings/profile.tsx
@@ -1,3 +1,49 @@
+import { For } from "solid-js";
+import createUserProfile from "~/hooks/user/createUserProfile";
+
+interface TitleAndValueProps {
+	title: string;
+	children: string;
+}
+
+function TitleAndValue(props: TitleAndValueProps) {
+	return (
+		<div class="space-y-2">
+			<div class="font-semibold text-xs opacity-75">
+				{props.title.toUpperCase()}
+			</div>
+			<div>{props.children}</div>
+		</div>
+	);
+}
+
 export default function SettingsInterfaceProfilePage() {
-	return "";
+	const userProfile = createUserProfile();
+
+	// TODO
+	const profileDataArray = [
+		{ children: userProfile().full_name, title: "Full Name" },
+		{ children: "22/1234", title: "Matric Number" },
+		{ children: "School of Computing", title: "Major / Department" },
+		{
+			children: userProfile().email ?? "blaa@student.babcock.edu.ng",
+			title: "School Email",
+		},
+		{ children: "Computer Science", title: "Course / Program of Study" },
+		{ children: "3rd year", title: "Year of Study" },
+	] as const satisfies TitleAndValueProps[];
+
+	return (
+		<div class="flex size-full flex-col rounded-box border border-base-300 bg-secondary p-4">
+			<h2 class="font-semibold">Profile Details</h2>
+			<div class="divider my-2" />
+			<div class="grid grid-cols-2 grid-rows-3 gap-6 p-2">
+				<For each={profileDataArray}>
+					{(val) => (
+						<TitleAndValue title={val.title}>{val.children}</TitleAndValue>
+					)}
+				</For>
+			</div>
+		</div>
+	);
 }
