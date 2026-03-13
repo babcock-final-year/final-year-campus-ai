d70ce0b1f90fbd31bb32e5929e41ce8a7fa3c500
Iceman8911 <wuchiJSS3@gmail.com>
2026-03-13 08:29:14 +0100

    fix(frontend, route): adjust settings interface spacing

--- apps/frontend/src/routes/home/settings.tsx
+++ apps/frontend/src/routes/home/settings.tsx
@@ -10,10 +10,10 @@ export default function SettingsInterfaceLayout(props: {
 		<div class="grid size-full grid-rows-[3.5rem_1fr]">
 			<HomeMainAreaHeader />
 
-			<div class="flex flex-col gap-4 overflow-auto bg-base-200 p-4 md:p-8">
+			<div class="flex flex-col gap-4 overflow-auto bg-base-200 p-4 sm:gap-6 md:p-6">
 				<SettingsProfileSummaryCard class="h-36" />
 
-				<div class="contents grow gap-4 sm:flex">
+				<div class="contents grow gap-4 sm:flex sm:gap-6">
 					<SettingsActionMenu class="w-full sm:w-60" />
 
 					<div class="grow">{props.children}</div>
