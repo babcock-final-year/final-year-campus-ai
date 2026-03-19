edf4d1508e36337976f3ee236585275d8d912e5c
Iceman8911 <wuchiJSS3@gmail.com>
2026-03-20 00:05:24 +0100

    refactor(frontend, hooks): remove redundant hooks

--- apps/frontend/src/hooks/chat/createUserChatHistory.ts
+++
@@ -1,8 +0,0 @@
-import { createAsync } from "@solidjs/router";
-import { getUserChatHistory } from "~/server/chat";
-
-export default function createUserChatHistory() {
-	const userChatHistory = createAsync(async () => getUserChatHistory());
-
-	return userChatHistory;
-}
