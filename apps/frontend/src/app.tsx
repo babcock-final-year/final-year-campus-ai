2835429f7f8a9e1fd8c370a4d91da4dfd444d99f
Iceman8911 <wuchiJSS3@gmail.com>
2026-03-19 20:46:41 +0100

    feat(frontend, context): add auth provider

--- apps/frontend/src/app.tsx
+++ apps/frontend/src/app.tsx
@@ -2,13 +2,16 @@ import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
+import { AuthProvider } from "./context/AuthContextProvider";
 
 export default function App() {
 	return (
 		<Router
 			root={(props) => (
 				<div class="flex h-screen w-screen items-center justify-center overflow-clip">
-					<Suspense>{props.children}</Suspense>
+					<AuthProvider>
+						<Suspense>{props.children}</Suspense>
+					</AuthProvider>
 				</div>
 			)}
 		>
---
+++ apps/frontend/src/context/AuthContextProvider.tsx
@@ -0,0 +1,51 @@
+import {
+	createContext,
+	createSignal,
+	type JSXElement,
+	type Setter,
+	useContext,
+} from "solid-js";
+import type { UserBaseOutput } from "~/models/users.schemas";
+import AuthRpc from "~/rpc/auth";
+
+interface AuthContextData {
+	accessToken: string | null;
+	setAccessToken: Setter<string | null>;
+	userProfile: UserBaseOutput | null;
+	setUserProfile: Setter<UserBaseOutput | null>;
+	logout(): Promise<void>;
+}
+
+const AuthContext = createContext<AuthContextData>();
+
+export function AuthProvider(props: { children: JSXElement }) {
+	const [accessToken, setAccessToken] = createSignal<string | null>(null);
+	const [userProfile, setUserProfile] = createSignal<UserBaseOutput | null>(
+		null,
+	);
+
+	async function logout() {
+		setAccessToken(null);
+		setUserProfile(null);
+
+		await AuthRpc.logout.post();
+	}
+
+	return (
+		<AuthContext.Provider
+			value={{
+				accessToken: accessToken(),
+				logout,
+				setAccessToken,
+				setUserProfile,
+				userProfile: userProfile(),
+			}}
+		>
+			{props.children}
+		</AuthContext.Provider>
+	);
+}
+
+export function useAuth() {
+	return useContext(AuthContext);
+}
