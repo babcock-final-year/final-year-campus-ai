713681ffc3a7dab182fb6947258e4b464890bd8d
Iceman8911 <wuchiJSS3@gmail.com>
2026-03-20 21:54:57 +0100

    refactor(frontend): move provider to highest level

--- apps/frontend/src/app.tsx
+++ apps/frontend/src/app.tsx
@@ -6,16 +6,16 @@ import { AuthProvider } from "./context/AuthContextProvider";
 
 export default function App() {
 	return (
-		<Router
-			root={(props) => (
-				<div class="flex h-screen w-screen items-center justify-center overflow-clip">
-					<AuthProvider>
+		<AuthProvider>
+			<Router
+				root={(props) => (
+					<div class="flex h-screen w-screen items-center justify-center overflow-clip">
 						<Suspense>{props.children}</Suspense>
-					</AuthProvider>
-				</div>
-			)}
-		>
-			<FileRoutes />
-		</Router>
+					</div>
+				)}
+			>
+				<FileRoutes />
+			</Router>
+		</AuthProvider>
 	);
 }
