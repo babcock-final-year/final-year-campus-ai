478c1eea58f09d44eed5eb95f0285d7e39b70253
Iceman8911 <wuchiJSS3@gmail.com>
2026-03-22 16:38:25 +0100

    fix(frontend, routes, components): mobile styling issue

--- apps/frontend/src/routes/home/settings/contact.tsx
+++ apps/frontend/src/routes/home/settings/contact.tsx
@@ -8,7 +8,7 @@ export default function SettingsInterfaceContactPage() {
 	return (
 		<div class="flex size-full p-4">
 			<Alert
-				class="alert alert-info h-fit grid-cols-[1.5rem_1fr] grid-rows-[1.5rem_1fr] bg-info/25"
+				class="alert alert-info h-fit max-w-full grid-cols-[1.5rem_1fr] grid-rows-[1.5rem_1fr] bg-info/25"
 				role="alert"
 			>
 				<Info class="col-start-1 row-start-1" />
@@ -17,7 +17,7 @@ export default function SettingsInterfaceContactPage() {
 					Contact Us Directly
 				</h2>
 
-				<p class="col-start-2 row-start-2">
+				<p class="wrap-break-word col-start-2 row-start-2 max-w-[62vw]">
 					For urgent academic inquiries or administrative issues, you can also
 					reach us directly at{" "}
 					<Link class="link link-primary" href={`mailto:${contactEmail}`}>
